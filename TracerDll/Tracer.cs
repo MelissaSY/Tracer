07572fd3c4fd7db6d689dbf6661705b5727e6384
Anna <annasy1308@gmail.com>
2022-09-19 09:26:58 +0300

    finishing TracerDll

--- TracerDll/Tracer.cs
+++ TracerDll/Tracer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics;
 using System.Collections.Concurrent;
+using System.Diagnostics.Tracing;
 
 namespace TracerDll
 {
@@ -13,33 +14,47 @@ namespace TracerDll
         {
             _threadResult = new ConcurrentDictionary<int, ThreadResult>();
             _startTime = new ConcurrentDictionary<int, ConcurrentStack<MethodResult>>();
-            _stopwatch = Stopwatch.StartNew();
+            _stopwatch = new Stopwatch();
         }
 
         public void StartTrace() 
         {
+            if(!_stopwatch.IsRunning)
+            {
+                _stopwatch.Start();
+            }
             int threadId = Thread.CurrentThread.ManagedThreadId;
-            if(!_startTime.ContainsKey(threadId))
+            //StackTrace stackTrace = new StackTrace(true);
+            StackFrame stackFrame = new StackFrame(1);
+            //add some checks here
+            MethodResult method = new MethodResult(stackFrame.GetMethod().DeclaringType.Name, stackFrame.GetMethod().Name);
+            if (!_startTime.ContainsKey(threadId))
             {
                 _startTime.TryAdd(threadId, new ConcurrentStack<MethodResult>());
                 _threadResult.TryAdd(threadId, new ThreadResult(threadId));
                 _threadResult[threadId].threadId = threadId;
                 _threadResult[threadId].childMethods = new ConcurrentBag<MethodResult>();
             }
-
+            _startTime[threadId].Push(method);
+            _threadResult[threadId].childMethods.Add(method);
+            method.time = _stopwatch.ElapsedMilliseconds;
         }
         public void StopTrace()
         {
             int threadId = Thread.CurrentThread.ManagedThreadId;
             MethodResult? method;
-            _startTime[threadId].TryPop(out method);
-            if(method != null)
+            if(_startTime.ContainsKey(threadId))
             {
-                method.time = _stopwatch.ElapsedMilliseconds - method.time;
+                _startTime[threadId].TryPop(out method);
+                if (method != null)
+                {
+                    method.time = _stopwatch.ElapsedMilliseconds - method.time;
+                }
             }
         }
         public TraceResult GetTraceResult()
         {
+            _traceResult = new TraceResult(_threadResult);
             return _traceResult;
         }
     }
